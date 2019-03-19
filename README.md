ec1e3c740757e7aec3c8b45c8d7b1ad2d013fa33
freewind <nowindlee@gmail.com>
2019-03-19 16:48:13 +0800

    demo

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-Webpack Config File Demo
-========================
+Webpack Config "esm-webpack-plugin" Demo
+========================================
 
-Put all settings in `webpack.config.js`
+webpack本身不支持输出esm模块的代码，使用`esm-webpack-plugin`可以解决这个问题。
 
 ```
 npm install
 npm run demo
 ```
 
-Then open `index.html` in your browser.
+注意：
 
-![demo](./images/demo.jpg)
+1. 要使用支持`import()`函数的浏览器，比如chrome
+2. webpack.config.js中的变量名`LIB`可以为任何一个没有跟代码中暴露出来的变量重名的名字
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,15 @@
+const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
+
 module.exports = {
-    mode: "development",
-    entry: './entry.js',
-    output: {
-        path: __dirname,
-        filename: 'dist/bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [
-                {loader: 'style-loader'},
-                {loader: 'css-loader'}
-            ]
-        }]
-    }
+  mode: "development",
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'dist/bundle.js',
+    library: "LIB",
+    libraryTarget: "var"
+  },
+  plugins: [
+    new EsmWebpackPlugin()
+  ]
 }
